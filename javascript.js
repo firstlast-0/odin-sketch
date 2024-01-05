0338f41a7d9b8f5565fb5402c5f366152a0fb636
Vincent <2w121q@gmail.com>
2024-01-05 09:51:12 +0800

    Add the 'NEW GRID' button

--- javascript.js
+++ javascript.js
@@ -1,18 +1,39 @@
-
-let cont = document.querySelector('#container');
-
-for (let i = 0; i < 16; i++) {
-    let div = document.createElement('div');
-    div.id = 'div' + i;
-    cont.appendChild(div);
-
-    let di = document.querySelector(`#div${i}`);
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement('div');        
-        di.appendChild(div);
-        div.addEventListener('mouseover', (e) => {
-            let d = e.target;
-            d.style.backgroundColor = 'black';
-        });
+function removeAllC(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
+
+let newG = document.querySelector('button');
+let cont = document.querySelector('#container');
+
+newG.addEventListener('click', (e) => {
+    let x = prompt('Enter the size of the grid', 0);
+    if (x < 1) {
+        alert('Grid size cannot be less than 1');
+    } else if (x > 100) {
+        alert('Grid size cannot be greater than 100');
+    } else {
+        removeAllC(cont);
+        drawGrid(x);
+    }        
+});
+
+function drawGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let div = document.createElement('div');
+        div.id = 'div' + i;
+        cont.appendChild(div);
+    
+        let di = document.querySelector(`#div${i}`);
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement('div');        
+            di.appendChild(div);
+            div.addEventListener('mouseover', (e) => {
+                let d = e.target;
+                d.style.backgroundColor = 'black';
+            });
+        }
+    }
+}
+drawGrid(16);
