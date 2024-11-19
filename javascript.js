73b22bb3283f2b5a4ba242826871a5e84d5d281b
firstlast-0 <127908930+firstlast-0@users.noreply.github.com>
2024-11-19 19:20:54 +0800

    Improve variable names

--- javascript.js
+++ javascript.js
@@ -1,26 +1,26 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 
-function removeAllC(parent) {
+function removeAllChildren(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-let newG = document.querySelector('button');
-let cont = document.querySelector('#container');
+let newGrid = document.querySelector('button');
+let container = document.querySelector('#container');
 
-newG.addEventListener('click', (e) => {
+newGrid.addEventListener('click', (e) => {
     let x = prompt('Enter the size of the grid', 0);
     if (x < 1) {
         alert('Grid size cannot be less than 1');
     } else if (x > 100) {
         alert('Grid size cannot be greater than 100');
     } else {
-        removeAllC(cont);
+        removeAllChildren(container);
         drawGrid(x);
-    }        
+    }
 });
 
 let opacity = 0.1;
@@ -28,15 +28,15 @@ function drawGrid(size) {
     for (let i = 0; i < size; i++) {
         let row = document.createElement('div');
         row.id = 'div' + i;
-        cont.appendChild(row);
-    
+        container.appendChild(row);
+
         let rowRef = document.querySelector(`#div${i}`);
         for (let j = 0; j < size; j++) {
-            let cell = document.createElement('div');        
+            let cell = document.createElement('div');
             rowRef.appendChild(cell);
             cell.addEventListener('mouseover', (event) => {
                 let hoveredCell = event.target;
-                let r = 0; // getRandomInt(255);
+                let r = 0; // getRandomInt(255)
                 let g = 0;
                 let b = 0;
                 hoveredCell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
