3b68c0405d5529095a39b0fc456ab4e70b6da3ed
firstlast-0 <127908930+firstlast-0@users.noreply.github.com>
2024-12-31 20:02:33 +0800

    Add feature to use Random colors & darkening

--- javascript.js
+++ javascript.js
@@ -10,21 +10,25 @@ function removeAllChildren(parent) {
 
 let newGrid = document.querySelector('button');
 let container = document.querySelector('#container');
+let opacity = 0.1;
 
 newGrid.addEventListener('click', (e) => {
-    let x = prompt('Enter the size of the grid', 0);
+    let x = prompt('Enter the size of the grid', 16);
+    let isRandomActive = prompt('Enter 0 to use black, 1 to use random colors', 0);
+    let isDarkeningActive = prompt('Enter 1 to enable Progressive Darkening Effect, 0 otherwise', 0);
+
     if (x < 1) {
         alert('Grid size cannot be less than 1');
     } else if (x > 100) {
         alert('Grid size cannot be greater than 100');
     } else {
         removeAllChildren(container);
-        drawGrid(x);
+        drawGrid(x, isRandomActive, isDarkeningActive);
+        opacity = 0.1;
     }
 });
 
-let opacity = 0.1;
-function drawGrid(size) {
+function drawGrid(size, isRandomActive, isDarkeningActive) {
     for (let i = 0; i < size; i++) {
         let row = document.createElement('div');
         row.id = 'div' + i;
@@ -36,14 +40,24 @@ function drawGrid(size) {
             rowRef.appendChild(cell);
             cell.addEventListener('mouseover', (event) => {
                 let hoveredCell = event.target;
-                let r = 0; // getRandomInt(255)
+
+                let r = 0;
                 let g = 0;
                 let b = 0;
+                if (+isRandomActive) {
+                    r = getRandomInt(255);
+                    g = getRandomInt(255);
+                    b = getRandomInt(255);
+                }
+                
                 hoveredCell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-                hoveredCell.style.opacity = opacity;
+                if (+isDarkeningActive) {
+                    hoveredCell.style.opacity = opacity;
+                }
                 opacity += 0.1;
             });
         }
     }
 }
-drawGrid(16);
+// drawGrid(16);
+newGrid.click();
